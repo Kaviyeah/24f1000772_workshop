b3e1707a8a8125b529c4e754bcd5f8ecd7bcd509
Kaviyeah <24f1000772@ds.study.iitm.ac.in>
2026-01-21 20:57:05 +0530

    changes made in factorial.py

--- armstrong.py
+++ armstrong.py
@@ -1,15 +1,12 @@
 num = int(input("Enter a number: "))
 
-temp = num
-sum = 0
 digits = len(str(num))
+total = 0
 
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** digits
-    temp //= 10
+for d in str(num):
+    total += int(d) ** digits
 
-if sum == num:
+if total == num:
     print(num, "is an Armstrong number")
 else:
     print(num, "is not an Armstrong number")
